8277170e0698919cd80ecdd3d0b924e4beb830cc
Adenle Abiodun <adenleabbey@hotmail.com>
2020-04-19 17:15:28 +0100

    Trivia app API integrated with frontend.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# Full Stack API Final Project
+# Full Stack Trivia API 
 
-## Full Stack Trivia
 
-Udacity is invested in creating bonding experiences for its employees and students. A bunch of team members got the idea to hold trivia on a regular basis and created a  webpage to manage the trivia app and play the game, but their API experience is limited and still needs to be built out. 
+## Introduction
+Flaskr Backend API gets the list of categories of questions in the trivia app.
+The Trivia questions are sorted into categories and also rated based on difficulty
 
-That's where you come in! Help them finish the trivia app so they can start holding trivia and seeing who's the most knowledgeable of the bunch. The application must:
+This API will give the functionality to:
 
 1) Display questions - both all questions and by category. Questions should show the question, category and difficulty rating by default and can show/hide the answer. 
 2) Delete questions.
@@ -12,33 +13,133 @@ That's where you come in! Help them finish the trivia app so they can start hold
 4) Search for questions based on a text query string.
 5) Play the quiz game, randomizing either all questions or within a specific category. 
 
-Completing this trivia app will give you the ability to structure plan, implement, and test an API - skills essential for enabling your future applications to communicate with others. 
 
-## Tasks
+## Overview
+This API majorly uses 3 methods as listed in the table below
+Most of the get requested do not require arguments.
+- Request Arguments: None
 
-There are `TODO` comments throughout project. Start by reading the READMEs in:
+You can read more about HTTP request methods
 
-1. [`./frontend/`](./frontend/README.md)
-2. [`./backend/`](./backend/README.md)
+# About the Stack
 
-We recommend following the instructions in those files in order. This order will look familiar from our prior work in the course.
+This is a  full stack application desiged with some key functional areas:
 
-## Starting and Submitting the Project
+## Backend
 
-[Fork](https://help.github.com/en/articles/fork-a-repo) the [project repository]() and [Clone](https://help.github.com/en/articles/cloning-a-repository) your forked repository to your machine. Work on the project locally and make sure to push all your changes to the remote repository before submitting the link to your repository in the Classroom. 
+The `./backend` directory contains a completed Flask and SQLAlchemy server. You will work primarily in app.py to define your endpoints and can reference models.py for DB and SQLAlchemy setup. It uses a Postgress database
 
-## About the Stack
+Full Documentation on how to use the API can be found here  [API Documentation](https://documenter.getpostman.com/view/4874547/Szf6WoLP)
+### Getting Started
 
-We started the full stack application for you. It is desiged with some key functional areas:
+### Installing Dependencies
 
-### Backend
+#### Python 3.7
 
-The `./backend` directory contains a partially completed Flask and SQLAlchemy server. You will work primarily in app.py to define your endpoints and can reference models.py for DB and SQLAlchemy setup. 
+Follow instructions to install the latest version of python for your platform in the [python docs](https://docs.python.org/3/using/unix.html#getting-and-installing-the-latest-version-of-python)
 
-### Frontend
+#### Virtual Enviornment
 
-The `./frontend` directory contains a complete React frontend to consume the data from the Flask server. You will need to update the endpoints after you define them in the backend. Those areas are marked with TODO and can be searched for expediency. 
+We recommend working within a virtual environment whenever using Python for projects. This keeps your dependencies for each project separate and organaized. Instructions for setting up a virual enviornment for your platform can be found in the [python docs](https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/)
 
-Pay special attention to what data the frontend is expecting from each API response to help guide how you format your API. 
+#### PIP Dependencies
 
-[View the README.md within ./frontend for more details.](./frontend/README.md)
+Once you have your virtual environment setup and running, install dependencies by naviging to the `/backend` directory and running:
+
+```bash
+pip install -r requirements.txt
+```
+
+This will install all of the required packages we selected within the `requirements.txt` file.
+
+##### Key Dependencies
+
+- [Flask](http://flask.pocoo.org/)  is a lightweight backend microservices framework. Flask is required to handle requests and responses.
+
+- [SQLAlchemy](https://www.sqlalchemy.org/) is the Python SQL toolkit and ORM we'll use handle the lightweight sqlite database. You'll primarily work in app.py and can reference models.py. 
+
+- [Flask-CORS](https://flask-cors.readthedocs.io/en/latest/#) is the extension we'll use to handle cross origin requests from our frontend server. 
+
+### Database Setup
+With Postgres running, restore a database using the trivia.psql file provided. From the backend folder in terminal run:
+```bash
+psql trivia < trivia.psql
+```
+
+### Running the server
+
+From within the `backend` directory first ensure you are working using your created virtual environment.
+
+To run the server, execute:
+
+```bash
+export FLASK_APP=flaskr
+export FLASK_ENV=development
+flask run
+```
+
+Setting the `FLASK_ENV` variable to `development` will detect file changes and restart the server automatically.
+
+Setting the `FLASK_APP` variable to `flaskr` directs flask to use the `flaskr` directory and the `__init__.py` file to find the application. 
+
+
+## Frontend
+
+The `./frontend` directory contains a complete React frontend to consume the data from the Flask server. 
+
+### Getting Setup
+
+> _tip_: this frontend is designed to work with [Flask-based Backend](../backend). It is recommended you stand up the backend first, test using Postman or curl, update the endpoints in the frontend, and then the frontend should integrate smoothly.
+
+### Installing Dependencies
+
+#### Installing Node and NPM
+
+This project depends on Nodejs and Node Package Manager (NPM). Before continuing, you must download and install Node (the download includes NPM) from [https://nodejs.com/en/download](https://nodejs.org/en/download/).
+
+#### Installing project dependencies
+
+This project uses NPM to manage software dependencies. NPM Relies on the package.json file located in the `frontend` directory of this repository. After cloning, open your terminal and run:
+
+```bash
+npm install
+```
+
+>_tip_: **npm i** is shorthand for **npm install**
+
+## Required Tasks
+
+## Running Your Frontend in Dev Mode
+
+The frontend app was built using create-react-app. In order to run the app in development mode use ```npm start```. You can change the script in the ```package.json``` file. 
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser. The page will reload if you make edits.<br>
+
+```bash
+npm start
+```
+
+## Authentication
+There are currently no authentication methods for this API. This will come in the subsequent versions.
+
+You can call these endpoints from your domain as CORS is enabled and you should not have issues getting responses.
+
+If there are issues, contact the details as seen at the Authors subheading.
+
+# Error Codes
+Flaskr Backend API uses standard error codes. Some of which are listed below with their meaning.
+
+| Error Code | Error Message |
+| ------- | ------ |
+| 200 | Succesful |
+| 404 | Not Found |
+| 405 | Method not allowed |
+| 422 | Unprocessable Entity |
+| 500 | Internat server error |
+
+
+## Rate limit
+There are currently no rate limits to the API calls for Flaskr Backend API
+
+## Author
+> Adenle Abiodun  adenleabbey@hotmail.com
--- starter/backend/flaskr/__init__.py
+++ starter/backend/flaskr/__init__.py
@@ -19,7 +19,6 @@ def pagination(request, selection):
     current_questions = questions[start:end]
     return current_questions
 
-
 def create_app(test_config=None):
     # creating an instance of the app and configuring it
     app = Flask(__name__, instance_relative_config=False)
@@ -37,8 +36,7 @@ def create_app(test_config=None):
         response.headers.add('Access-Control_Allow-Methods',
                              'GET, POST, PATCH, DELETE, OPTIONS')
         return response
-    '''This api gets the list of categories of questions in the trivia app
-    via a GET method and also shows the total count of the categories'''
+   
 
     @app.route('/api/categories')
     def get_categories():
@@ -105,7 +103,6 @@ def create_app(test_config=None):
 
             def previous_page(page):
                 if page == 1:
-                    print("YESSS")
                     return
                 else:
                     return "http://127.0.0.1:5000/api/questions?page=" + str(int(page)-1)+""
@@ -126,15 +123,31 @@ def create_app(test_config=None):
             abort(404)
 
     # '''
-    # @TODO:
+    # COMPLETED:
     # Create an endpoint to DELETE question using a question ID.
 
     # TEST: When you click the trash icon next to a question, the question will be removed.
     # This removal will persist in the database and when you refresh the page.
     # '''
-
+    @app.route('/api/questions/<int:question_id>', methods=['DELETE'])
+    def delete_question(question_id):
+        category = request.args.get('category', 1, type=int)
+        try:
+            question = Question.query.filter(
+                Question.id == question_id).one_or_none()
+            if question is None:
+                abort(422)
+            # Delete this question
+            question.delete()
+            response = {
+                "success": True,
+                "message": "Deleted"
+            }
+            return jsonify(response)
+        except():
+            abort(400)
     # '''
-    # @TODO:
+    # COMPLETED:
     # Create an endpoint to POST a new question,
     # which will require the question and answer text,
     # category, and difficulty score.
@@ -143,7 +156,35 @@ def create_app(test_config=None):
     # the form will clear and the question will appear at the end of the last page
     # of the questions list in the "List" tab.
     # '''
+    @app.route('/api/questions', methods=['POST', 'PUT'])
+    def create_question():
+        body = request.get_json()
 
+        new_question = body.get('question', None)
+        new_difficulty = body.get('difficulty', None)
+        new_category = body.get('category', None)
+        new_answer = body.get('answer', None)
+
+        if new_question is None:
+            abort(422)
+
+        try:
+            question = Question(
+                question=new_question,
+                answer=new_answer,
+                difficulty=new_difficulty,
+                category=new_category
+            )
+            question.insert()
+            response = {
+                "success": True,
+                "created": question.id,
+                "message": "Question created"
+            }
+
+            return jsonify(response)
+        except():
+            abort(422)
     # '''
     # @TODO:
     # Create a POST endpoint to get questions based on a search term.
@@ -155,30 +196,170 @@ def create_app(test_config=None):
     # Try using the word "title" to start.
     # '''
 
-    # '''
-    # @TODO:
-    # Create a GET endpoint to get questions based on category.
+    @app.route('/api/questions/search', methods=['POST'])
+    def search_questions():
+        body = request.get_json()
+        print(body.get("search_term"))
+        search_query = Question.query.filter(
+            Question.question.ilike('%' + body.get("search_term") + '%'))
+        print(search_query)
+        results = list(map(Question.format, search_query))
 
+        search_term = body.get("search_term", None)
+        if search_term is None:
+            abort(422)
+
+        response = {
+            "questions": results,
+            "total_questions": len(results),
+            "success": True
+        }
+
+        return jsonify(response)
+
+    # DONE: Create a GET endpoint to get questions based on category.
+    #
     # TEST: In the "List" tab / main screen, clicking on one of the
     # categories in the left column will cause only questions of that
     # category to be shown.
-    # '''
-    # '''
-    # @TODO:
-    # Create a POST endpoint to get questions to play the quiz.
+    @app.route('/api/categories/<int:category_id>/questions', methods=['GET'])
+    def category_questions(category_id):
+        try:
+            selection = Question.query.filter(
+                Question.category == category_id).all()
+            current_questions = pagination(request, selection)
+            total_questions = len(selection)
+            if len(current_questions) == 0:
+                abort(404)
+            current_category = Category.query.filter(
+                Category.id == category_id).one_or_none().format()
+            response = {
+                "success": True,
+                "questions": current_questions,
+                "total_questions": total_questions,
+                "current_category": current_category
+            }
+            return jsonify(response)
+
+        except():
+            abort(404)
+
+    # DONE: Create a POST endpoint to get questions to play the quiz.
     # This endpoint should take category and previous question parameters
     # and return a random questions within the given category,
     # if provided, and that is not one of the previous questions.
-
+    #
     # TEST: In the "Play" tab, after a user selects "All" or a category,
     # one question at a time is displayed, the user is allowed to answer
     # and shown whether they were correct or not.
-    # '''
+    @app.route('/api/quizzes', methods=['GET', 'POST'])
+    def play_quizzes():
+        body = request.get_json()
+        print(body)
+        category = body.get("quiz_category").get('id')
+        previous_questions = body.get("previous_questions", [])
+        print("Previous Questions: ", previous_questions)
+        try:
+            # gather all questions and their ids
+            if category == 0:
+                questions = Question.query.filter(
+                    Question.question != '').order_by(Question.id).all()
+            else:
+                questions = Question.query.filter(
+                    Question.category == category and Question.question != '').order_by(Question.id).all()
 
-    # '''
-    # @TODO:
-    # Create error handlers for all expected errors
+            if len(questions) == 0:
+                abort(404)
+            categoryname = Category.query.filter(Category.id == category).all()
+            categoryname = [categoryname.format() for categoryname in categoryname]
+            def showCategory():
+                if category == 0:
+                    return "All Categories"
+                else:
+                    return categoryname[0].get('type')
+            questions = [questions.format() for questions in questions]
+            total_questions = len(questions)
+            # Need to gather all questions in a selection and for each one,
+            # skip the previous ones by previous_question ids
+            i = 0
+            if len(previous_questions):
+                for question in questions:
+                    question_id = question["id"]
+                    for previous_id in previous_questions:
+                        if question_id == previous_id:
+                            del questions[i]
+                    i = i + 1
+
+            # Note the number of questions left
+            number_of_asked_questions = len(previous_questions)
+
+            # Randomize the question that comes up everytime you play if
+            # number of asked questions is equal to total questions minus
+            # asked questions means no random choice
+            random_max = (total_questions - number_of_asked_questions) - 1
+
+            # else randomize the question. keep it within the range of index.
+            if random_max > 0:
+                random_choice = random.randint(0, random_max)
+            else:
+                random_choice = 0
+
+            # Need to check if there are any questions left
+            # return False if no questions left to ask.
+            if number_of_asked_questions:
+                if number_of_asked_questions == total_questions:
+                    current_question = False
+                else:
+                    current_question = questions[random_choice]
+            else:
+                current_question = questions[random_choice]
+
+            response = {
+                "success": True,
+                "previousQuestions": previous_questions,
+                "question": current_question,
+                "questions": questions,
+                "total_questions": total_questions,
+                "current_category": showCategory()
+            }
+            print("Response: ", response)
+            return jsonify(response)
+
+        except():
+            abort(503)
+
+    # DONE: Create error handlers for all expected errors
     # including 404 and 422.
-    # '''
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "Not found"
+        }), 404
+
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        return jsonify({
+            "success": False,
+            "error": 405,
+            "message": "Method NOT allowed"
+        }), 405
+
+    @app.errorhandler(422)
+    def unprocessable_entity(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "Unprocessable Entity"
+        }), 422
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "Internal Server Error"
+        }), 500
 
     return app
